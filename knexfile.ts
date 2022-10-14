1d58fb242cf917f9c168e12586d06643915c48d4
Taiwo <ayeola05@gmail.com>
2022-10-14 05:48:53 +0100

    configured app for deployment

--- knexfile.ts
+++ knexfile.ts
@@ -39,20 +39,21 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 
 };
